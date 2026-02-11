62eacd6166c1b71db766daec81b493c65933d945
q8xj9gs8hs-a11y <243439570+q8xj9gs8hs-a11y@users.noreply.github.com>
2026-02-10 23:42:27 -0500

    Delete fabric-mcp directory

--- fabric-mcp/src/fabric_mcp/__about__.py
+++
@@ -1,3 +0,0 @@
-"Version information for fabric_mcp."
-
-__version__ = "1.0.3"
--- fabric-mcp/src/fabric_mcp/__init__.py
+++
@@ -1,5 +0,0 @@
-"Connect Fabric AI framework to Model Context Protocol (MCP) compatible applications."
-
-from .__about__ import __version__
-
-__all__ = ["__version__"]
